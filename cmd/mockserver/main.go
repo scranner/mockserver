2c63dd8cec9da5f021416cae34d58485b488214b
Scranner <zeldas-link@hotmail.co.uk>
2020-12-19 16:56:42 +0000

    initial commit

---
+++ cmd/mockserver/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/scranner/mockserver/pkg/cmd"
+	"github.com/scranner/mockserver/pkg/cmd/mockserver"
+)
+
+func main() {
+	print("Test")
+}
---
+++ pkg/mockserver/mock-server.go
@@ -0,0 +1,5 @@
+package mockserver
+
+func NewMockServer(config string) {
+
+}
