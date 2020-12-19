0fc5d7a15c0fdb2ea2eec958976139dd7d0f37f8
Scranner <zeldas-link@hotmail.co.uk>
2020-12-19 18:04:17 +0000

    initial commit

--- cmd/mockserver/main.go
+++ cmd/mockserver/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/scranner/mockserver/pkg/cmd"
 	"github.com/scranner/mockserver/pkg/cmd/mockserver"
 )
 
 func main() {
-	mockserver
+	MockServer := mockserver.NewMockServer()
+	go MockServer.StartServer()
 }
--- pkg/mockserver/mock-server.go
+++ pkg/mockserver/mock-server.go
@@ -1,19 +1,57 @@
 package mockserver
 
-type mockServer struct {
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
 
+// Server Object
+type Server struct {
+	Router *mux.Router
+	Config *Config
 }
 
-type mockServerConfig struct {
-
+// Config Server Config
+type Config struct {
+	Port string
 }
 
 // NewMockServer Creates new mock http server based on supplied
 // JSON config
-func NewMockServer(config string) *mockServer {
-	return &mockServer{}
+func NewMockServer(suppliedConfig string) Server {
+	router := mux.NewRouter()
+	router.HandleFunc("/live", probeHandler)
+	router.HandleFunc("/ready", probeHandler)
+
+	return Server{
+		router,
+		parseConfig(suppliedConfig),
+	}
 }
 
-func parseConfig(config string) *mockServerConfig {
-	return &mockServerConfig{}
+// StartServer will start the http server
+func (s *Server) StartServer() {
+	srv := &http.Server{
+		Handler:      s.Router,
+		Addr:         fmt.Sprintf("127.0.0.1:%s", s.Config.Port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func parseConfig(unparsedConfig string) *Config {
+	return &Config{
+		"8080",
+	}
+}
+
+func probeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "OK")
 }
