37a9f02a413323d2c659625de09a537472bc487c
Scranner <zeldas-link@hotmail.co.uk>
2020-12-19 17:05:10 +0000

    initial commit

--- cmd/mockserver/main.go
+++ cmd/mockserver/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	mockserver.New
+	mockserver
 }
--- pkg/mockserver/mock-server.go
+++ pkg/mockserver/mock-server.go
@@ -1,6 +1,19 @@
 package mockserver
 
+type mockServer struct {
 
-func NewMockServer(config string) {
-	
+}
+
+type mockServerConfig struct {
+
+}
+
+// NewMockServer Creates new mock http server based on supplied
+// JSON config
+func NewMockServer(config string) *mockServer {
+	return &mockServer{}
+}
+
+func parseConfig(config string) *mockServerConfig {
+	return &mockServerConfig{}
 }
